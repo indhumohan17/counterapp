d9718e1c77d52c8cb0c3199b1bef2361be4a3933
sathish <indhumohan17@gmail.com>
2025-05-22 01:07:42 -0700

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
 
   return (
     <div >
-      <div className='color-container ' style={{  top: '4rem', left: '34rem' }}> <h1 style={{ position: 'absolute', marginLeft: '40%',marginTop: '4rem' }}>Counter App</h1>
+      <div className='color-container ' style={{  top: '4rem', left: '34rem' }}> <h1 style={{ position: 'absolute', marginLeft: '45%',marginTop: '4rem' }}>Counter App</h1>
 
       </div>
        <div className="container d-flex justify-content-center align-items-center" style={{ minHeight: '100vh' }}>
@@ -30,10 +30,10 @@ function App() {
       <div className="card card-animate text-center shadow" style={{ width: '22rem', padding: '2rem' ,position:'relative'}}>
         
         <h1 ref={counterRef} className='mt-3 mb-4'>{count}</h1>
-       
-        <button onClick={() => handleSetCount(count + 1)} className="btn btn-primary card m-2" style={{ width: '8rem', borderRadius:'0.2rem' }}>Increment</button>
-        <button onClick={() => handleSetCount(count - 1)} className="btn btn-dark card m-2" style={{ width: '8rem' , borderRadius:'0.2rem',position:'absolute', bottom:'8.6rem', left:'10.7rem' }}>Decrement</button>
-        <button onClick={() => handleSetCount(0)} className="btn btn-success card mt-3" style={{ width: '15rem', borderRadius:'0.2rem', marginLeft:'1.2rem' }}>Reset</button>
+
+        <button onClick={() => handleSetCount(count + 1)} className="btn btn-primary card m-2" style={{ width: '8rem', borderRadius:'0.2rem', bottom:'1.7rem', top:'2rem' }}>Increment</button>
+        <button onClick={() => handleSetCount(count - 1)} className="btn btn-dark card m-2" style={{ width: '8rem' , borderRadius:'0.2rem', left:'8.7rem', bottom:'1.7rem', }}>Decrement</button>
+        <button onClick={() => handleSetCount(0)} className="btn btn-success  mt-3" style={{ width: '15rem', borderRadius:'0.2rem', marginLeft:'1.2rem' }}>Reset</button>
          <p style={{  fontSize: '1.2rem', color: '#555', textShadow: '1px 1px 2px #ccc' }}>
         <span style={{ fontStyle: 'italic', color: '#888', bottom: '0', position: 'relative', top: '1rem',  }}>  (Pssst, you can change this value)</span>
 
